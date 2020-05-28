54577c61edd9139320821a304d4e7bf1e9657402
Kelvin Okornoe <kelvin.okornoe@gmail.com>
2020-05-28 08:07:06 +0000

    add implementation of number method for the selection of long type by the user

--- Sorting Tool/task/src/sorting/Main.java
+++ Sorting Tool/task/src/sorting/Main.java
@@ -5,7 +5,20 @@ import java.util.Collections;
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(final String[] args) {
+        //dataTypeSelection(args[1]);
+    }
+
+/*
+    // Method for reading the of words in the user input
+    private static void word () {
+
+    }
+*/
+
+    //Method for reading the of numbers/long in the user input
+    private static void number () {
         Scanner scanner = new Scanner(System.in);
         ArrayList<Long> numberList = new ArrayList();
         int index = 0;
@@ -24,6 +37,27 @@ public class Main {
         }
         System.out.println("Total numbers: " + listSize + ".");
         System.out.println("The greatest number: " + numberList.get(listSize - 1) + " (" + index + " time(s)).");
+    }
+/*
+
+    //Method for reading the number of lines in the user input
+    private static void line () {
 
     }
+*/
+
+/*    private static void dataTypeSelection (String dataType) {
+
+        switch (dataType) {
+            case "line":
+                line();
+                break;
+            case "long":
+                number();
+                break;
+            default:
+                word();
+                break;
+        }
+    }*/
 }
